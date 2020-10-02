3499e97863d32c3b6c5b0a34af5065b1ca7b929d
Tristan Eastman <teastman85@gmail.com>
2020-10-02 13:41:39 -0400

    Connect apiRoutes to server.

--- Develop/public/assets/js/apiRoutes.js
+++ Develop/public/assets/js/apiRoutes.js
@@ -1,11 +1,18 @@
 let noteData = require("../../../db/db.json");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
+
+        res.json(noteData);
+
         fs.readFileSync(path.join(__dirname, "../../../db/db.json"));
+        console.log("testNotes")
     })
 
     app.post("/api/notes", function (req, res) {
+        console.log("testNoteAdd")
         noteData.push(req.body);
 
         function addIdentifier(target) {
@@ -14,6 +21,7 @@ module.exports = function (app) {
         }
 
         function loop(noteData) {
+            console.log("testLoop")
             for (var i in noteData) {
                 var c = noteData[i];
                 if (typeof c === 'object') {
